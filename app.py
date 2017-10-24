263ecc0350c7d9e834e64ac6d2c450b1c6876738
Ethan Swan <eswan18@rocketmail.com>
2017-10-24 21:55:53 +0000

    working bootstrap integration

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-import flask
+import flask, flask_bootstrap
 
 app = flask.Flask(__name__)
+bootstrap = flask_bootstrap.Bootstrap(app)
 
 @app.route('/')
 def index():
@@ -9,6 +10,10 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return flask.render_template('user.html', name=name)
+
+@app.route('/user2/<name>')
+def user2(name):
+    return flask.render_template('user2.html', name=name)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
