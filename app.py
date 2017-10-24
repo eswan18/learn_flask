d403060464c5022ee32bde67cd4dbda24a69fd37
Ethan Swan <eswan18@rocketmail.com>
2017-10-24 21:17:24 +0000

    added first templates

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+import flask
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return flask.render_template('index.html')
+
+@app.route('/user/<name>')
+def user(name):
+    return flask.render_template('user.html', name=name)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
