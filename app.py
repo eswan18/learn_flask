4f6be3352743118d0e9c8e278f950f8829a69b20
Ethan Swan <eswan18@rocketmail.com>
2017-10-27 22:38:43 +0000

    added WTForm

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
-import flask, flask_bootstrap, flask_moment
+import flask, flask_bootstrap, flask_moment, flask_wtf, wtforms
 from datetime import datetime
 
 app = flask.Flask(__name__)
+app.config['SECRET_KEY'] = 'dummy_key'
 bootstrap = flask_bootstrap.Bootstrap(app)
 moment = flask_moment.Moment(app)
 
+# Name Form
+class NameForm(flask_wtf.FlaskForm):
+    name = wtforms.StringField('What is your name?',
+                                validators=[wtforms.validators.Required()])
+    submit = wtforms.SubmitField('Submit')
 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return flask.render_template('index.html', current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return flask.render_template('index.html', form=form, name=name)
 
 @app.route('/user/<name>')
 def user(name):
