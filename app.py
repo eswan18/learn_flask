1f0bfea8bb25cef2d411e2f243178cb1b3c9f0bc
Ethan Swan <eswan18@rocketmail.com>
2017-10-28 17:44:15 +0000

    added flashing message

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import flask, flask_bootstrap, flask_moment, flask_wtf, wtforms
+from flask import Flask, session, redirect, url_for, render_template, flash
 from datetime import datetime
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 app.config['SECRET_KEY'] = 'dummy_key'
 bootstrap = flask_bootstrap.Bootstrap(app)
 moment = flask_moment.Moment(app)
@@ -15,24 +16,28 @@ class NameForm(flask_wtf.FlaskForm):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
         form.name.data = ''
-    return flask.render_template('index.html', form=form, name=name)
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form,
+                                 name=session.get('name'))
 
 @app.route('/user/<name>')
 def user(name):
-    return flask.render_template('user.html', name=name)
+    return render_template('user.html', name=name)
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return flask.render_template('404.html'), 404
+    return render_template('404.html'), 404
 
 @app.errorhandler(500)
 def internal_server_error(e):
-    return flask.render_template('500.html'), 500
+    return render_template('500.html'), 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
